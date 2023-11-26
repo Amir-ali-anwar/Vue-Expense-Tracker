b9f48506d57987a390565845318732c47baeb295
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-26 20:14:23 +0500

    working on the expense and icome prop

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,11 @@
   <h3>History</h3>
   <ul id="list" class="list">
     <li :class="transaction.amount < 0 ? 'minus' : 'plus'" v-for="transaction in transactions" :key="transaction.id">
-        {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn" @click="deleteTransaction(transaction.id)">x</button>
+      {{ console.log({transaction})
+       }}
+      <p v-if="transaction.length>0">
+       {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn" @click="deleteTransaction(transaction.id)">x</button></p>
+       <p v-else>No Transaction to Show</p>
       </li>
   </ul>
 </template>
